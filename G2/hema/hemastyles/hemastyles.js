4c9821a6bbe472832c8203f78a4bba3153b91443
prabhsaran <prabhvirsaran@gmail.com>
2017-03-08 22:02:11 -0800

    now using jquery to hide or show divs on the styles page

---
+++ G2/hema/hemastyles/hemastyles.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+    $('#one').click(function () {
+        $('#default').replaceWith($('#longsword').removeClass('hidden').addClass('styleDescriptions'));
+        $('#rapier, #sidesword, #wrestling, #other').addClass('hidden');
+        
+    });
+
+    $('#two').click(function () {
+        $('#default').replaceWith($('#rapier').removeClass('hidden').addClass('styleDescriptions'));
+        $('#longsword, #sidesword, #wrestling, #other').addClass('hidden');
+        
+    });
+
+    $('#three').click(function () {
+        $('#default').replaceWith($('#sidesword').removeClass('hidden').addClass('styleDescriptions'));
+        $('#longsword, #rapier, #wrestling, #other').addClass('hidden');
+    });
+
+    $('#four').click(function () {
+        $('#default').replaceWith($('#wrestling').removeClass('hidden').addClass('styleDescriptions'));
+        $('#longsword, #rapier, #sidesword, #other').addClass('hidden');
+    });
+
+    $('#five').click(function () {
+        $('#default').replaceWith($('#other').removeClass('hidden').addClass('styleDescriptions'));
+        $('#longsword, #rapier, #sidesword, #wrestling').addClass('hidden');
+    });
+
+
+});
