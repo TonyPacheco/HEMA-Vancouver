4c4e1e0c46d9b19a6c1fd19fefc37293b0a2ad01
Prabhvir Saran <Prabhvir Saran>
2017-03-23 18:42:23 -0700

    added tooltip and milestone 4

--- G2/login/loginorsignup-validate.js
+++ G2/login/loginorsignup-validate.js
@@ -124,3 +124,25 @@ document.getElementById("logpassword").onchange = checkLogPassword;
 document.getElementById("regpassword").onchange = checkRegPassword;
 document.getElementById("confpassword").onchange = checkConfPassword;
 document.getElementById("confemail").onchange = checkConfEmail;
+
+$(document).ready(function() {
+
+$('.masterTooltip').hover(function(){ 
+        var title = $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+		
+		
+}, function() {
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+}).mousemove(function(e) {
+        var mousex = e.pageX + 20; //Get X coordinates
+        var mousey = e.pageY + 10; //Get Y coordinates
+        $('.tooltip')
+        .css({ top: mousey, left: mousex })
+});
+});
